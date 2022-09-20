b03f3b10fda7fd3ddc9c9b5ca27e909f6ecabfd7
SAMIR RIBEIRO MACARIO <114541@pmenos.com.br>
2022-09-20 11:40:31 -0300

    lamnda desliga ambiente e teste rede

---
+++ aws-scheduler/README.md
@@ -0,0 +1,27 @@
+# Gatilho do lambda
+
+EventBridge (CloudWatch Events): check-scheduler-event
+arn:aws:events:us-east-1:123123123123:rule/check-scheduler-event
+Detalhes
+Barramento de evento: default
+Descrição: check-scheduler-event
+Expressão de programação: cron(5 * * * ? *)
+name: check-scheduler-event
+Service principal: events.amazonaws.com
+Statement ID: AllowExecutionFromCloudWatch
+url: events/home#/rules/check-scheduler-event
+
+
+
+Destino: 
+Serviço da AWS
+
+Função lambda
+
+	Set função
+	
+	
+Tipo de regra
+	Regra programada
+Barramento de eventos
+	default
---
+++ teste-rede/teste-rede-lambda.js
@@ -0,0 +1,39 @@
+const https = require('https');
+function getRequest() {
+  const url = 'https://reqres.in/api/users';
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, res => {
+      let rawData = '';
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(rawData));
+        } catch (err) {
+          reject(new Error(err));
+        }
+      });
+    });
+    req.on('error', err => {
+      reject(new Error(err));
+    });
+  });
+}
+exports.handler = async event => {
+  try {
+    const result = await getRequest();
+    console.log('result is: 👉️', result);
+    return {
+      statusCode: 200,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log('Error is: 👉️', error);
+    return {
+      statusCode: 400,
+      body: error.message,
+    };
+  }
+};
